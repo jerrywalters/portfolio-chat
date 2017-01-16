59037f5a00342ba56fc3e63e8f8a089fc8eb58ba
Jerry <jerrydwalters@gmail.com>
2017-01-16 16:44:15 -0500

    :tada: initial commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyA1Db7hSJ658255QfdZ09mjJR4VxcXbDP8",
+    authDomain: "portfolio-chat.firebaseapp.com",
+    databaseURL: "https://portfolio-chat.firebaseio.com",
+    storageBucket: "portfolio-chat.appspot.com",
+    messagingSenderId: "892675563096"
+  };
+
+  var database = firebase.database();
+
+
+  firebase.initializeApp(config);
