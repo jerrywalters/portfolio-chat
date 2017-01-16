9b0d6e97a8ecce7bafde5683f37cd18e3807a7bd
Jerry <jerrydwalters@gmail.com>
2017-01-16 17:25:23 -0500

    add firebase, push message to firebase, retrieve messages

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+
+// initialize firebase
 var config = {
     apiKey: "AIzaSyA1Db7hSJ658255QfdZ09mjJR4VxcXbDP8",
     authDomain: "portfolio-chat.firebaseapp.com",
@@ -6,7 +8,28 @@ var config = {
     messagingSenderId: "892675563096"
   };
 
-  var database = firebase.database();
+firebase.initializeApp(config);
 
 
-  firebase.initializeApp(config);
+var chatDb = firebase.database().ref('messages');
+
+// write new messages to db
+$('.chat').submit(function(e) {
+  e.preventDefault();
+
+  var message = $('.chat__input').val();
+  console.log('message sent:', message);
+  chatDb.push({
+    message: message,
+    createdOn: Date.now()
+  }, function(){
+    $(".chat__input").val(''); // clear input
+  });
+});
+
+// read from db and write to page
+chatDb.on('child_added', function(data) {
+  console.log('new message!');
+  var newMessage = data.val().message;
+  $('.messages__list').append('<li>' + newMessage + '</li>');
+});
