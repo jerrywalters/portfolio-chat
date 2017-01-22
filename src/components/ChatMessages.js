a34ad738018429a2d9280f0a92e840a4a975ddea
Jerry <jerrydwalters@gmail.com>
2017-01-22 14:14:51 -0500

    removed excess dependencies and files

--- src/components/ChatMessages.js
+++ src/components/ChatMessages.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ChatMessages = ({messages}) => {
   let messageList = messages.map(
-    (message, index) => <li key={index}>{message.message + ', milord'} </li>
+    (message, index) => <li key={index}>{message.message + ', milord.'} </li>
   )
   return (
     <div>
